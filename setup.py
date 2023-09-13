64e0de79fd1290f9388c7cdf44200d42399f8500
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2023-09-13 06:27:01 +0200

    Updated versioning files

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
+import os
+import re
+
 from distutils.core import setup
 
+ROOT = os.path.dirname(__file__)
+VERSION_RE = re.compile(r'''__version__ = ['"]([0-9.]+)['"]''')
+
+requires = [
+  'pulumi-kubernetes>=4.1.1,<5.0.0',
+  'pulumi>=3.82.0,<4.0.0',
+],
+
+def get_version():
+    init = open(os.path.join(ROOT, 'python_pulumi_helm', '__init__.py')).read()
+    return VERSION_RE.search(init).group(1)
+  
 setup(name='python-pulumi-helm',
-      version='0.1.7',
+      version=get_version(),
       description='Python Pulumi Helm charts',
       url='https://github.com/luismiguelsaez/python-pulumi-helm',
-      install_requires=[
-        'pulumi-kubernetes>=4.1.1,<5.0.0',
-        'pulumi>=3.82.0,<4.0.0',
-      ],
+      install_requires=requires
 )
