3b2ac39b6431e1115145c88ce7429c06157866ca
Luis Miguel Sáez Martín <luismiguelsaez83@gmail.com>
2023-11-21 17:39:25 +0100

    Fix format

--- python_pulumi_helm/releases.py
+++ python_pulumi_helm/releases.py
@@ -672,7 +672,7 @@ def argocd(
                 "name": "cmp-kustomize-aws-secretsmanager"
             },
             "data": {
-                "plugin.yaml": "apiVersion: argoproj.io/v1alpha1\nkind: ConfigManagementPlugin\nmetadata:\n  name: cmp-kustomize-aws-secretsmanager\nspec:\n  version: v1.0\n  init:\n    command: ['sh', '-c']\n    args: ['find . -type f -name kustomization.yaml']\n  generate:\n    command: ['sh', '-c']\n    args: [ 'kustomize build . | argocd-vault-plugin generate --verbose-sensitive-output -' ]\n  discover:\n    find:\n      glob: './**/kustomization.yaml'\n"
+                "plugin.yaml": "apiVersion: argoproj.io/v1alpha1\nkind: ConfigManagementPlugin\nmetadata:\n  name: cmp-kustomize-aws-secretsmanager\nspec:\n  version: v1.0\n  init:\n    command: [sh, -c]\n    args:\n        - echo\n  generate:\n    command: [sh, -c]\n    args:\n        - kustomize\n        - build\n        - .\n        - |\n        - argocd-vault-plugin\n        - generate\n        - --verbose-sensitive-output\n        - -\n  discover:\n    find:\n      glob: ./**/kustomization.yaml\n"
             }
         }
     ]
